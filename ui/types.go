6d4cc4c3713b63309501bede734476e52a298280
Oleksandr <ku@grade.ua>
2024-11-06 15:20:06 +0200

    create pixel canvas structure/rendering/layout

--- ui/types.go
+++ ui/types.go
@@ -3,10 +3,12 @@ package ui
 import (
 	"fyne.io/fyne/v2"
 	"pixl-ztm/apptype"
+	"pixl-ztm/pxcanvas"
 	"pixl-ztm/swatch"
 )
 
 type AppInit struct {
+	PixlCanvas *pxcanvas.PxCanvas
 	PixlWindow fyne.Window
 	State      *apptype.State
 	Swatches   []*swatch.Swatch
